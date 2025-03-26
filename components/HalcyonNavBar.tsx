476800b9faf048114c13cd6c711696be3137583f
fenriskanalytics <brandon@epifronlegacypartners.com>
2025-03-26 19:14:26 -0400

    m

--- components/HalcyonNavBar.tsx
+++ components/HalcyonNavBar.tsx
@@ -1,45 +1,49 @@
 'use client';
 
 import * as React from 'react';
+import { useRouter } from 'next/navigation';
 import { PlasmicHalcyonNavBar } from './plasmic/halcyon_website/PlasmicHalcyonNavBar';
 import type { PlasmicHalcyonNavBar__OverridesType } from './plasmic/halcyon_website/PlasmicHalcyonNavBar';
 
-type HalcyonNavBarProps = PlasmicHalcyonNavBar__OverridesType &
-  React.ComponentProps<typeof PlasmicHalcyonNavBar>;
+type HalcyonNavBarProps = PlasmicHalcyonNavBar__OverridesType & React.ComponentProps<typeof PlasmicHalcyonNavBar>;
 
 const HalcyonNavBar_ = (
   props: HalcyonNavBarProps,
   ref: React.Ref<HTMLDivElement>
 ) => {
+  const router = useRouter();
+
   return (
     <PlasmicHalcyonNavBar
       root={{ ref }}
       {...props}
-      loginLink={{
-        props: {
-          'data-mariana-auth-link': true,
-          'data-mariana-logged-out-text': 'Login',
-          'data-mariana-logged-in-text': 'Account',
-          href: '/account',
-          onClick: (e: React.MouseEvent) => {
-            e.preventDefault();
-            window.location.href = 'https://www.halcyonstudio.us/account/';
+      overrides={{
+        loginLink: {
+          props: {
+            'data-mariana-auth-link': true,
+            'data-mariana-logged-out-text': 'Login',
+            'data-mariana-logged-in-text': 'Account',
+            href: '/account',
+            onClick: (e: React.MouseEvent) => {
+              e.preventDefault();
+              router.push('/account');
+            },
           },
         },
-      }}
-      membershipsLink={{
-        props: {
-          onClick: (e: React.MouseEvent) => {
-            e.preventDefault();
-            window.location.href = 'https://www.halcyonstudio.us/memberships/buy/';
+        membershipsLink: {
+          props: {
+            onClick: (e: React.MouseEvent) => {
+              e.preventDefault();
+              router.push('/memberships/buy');
+            },
           },
         },
-      }}
-      scheduleLink={{
-        props: {
-          onClick: (e: React.MouseEvent) => {
-            e.preventDefault();
-            window.location.href = 'https://www.halcyonstudio.us/schedule/';
+        scheduleLink: {
+          props: {
+            onClick: (e: React.MouseEvent) => {
+              e.preventDefault();
+              router.push('/schedule');
+            },
           },
         },
       }}
